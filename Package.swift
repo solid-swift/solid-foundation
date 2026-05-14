d42fa65a26a48fe791fe668210099bb88f9746f6
Kevin Wooten <kevin@wooten.com>
2026-05-14 08:50:08 -0700

    Fix SolidIO package product mapping

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   products: [
     .library(name: "Solid", targets: ["Solid"]),
     .library(name: "SolidCore", targets: ["SolidCore"]),
-    .library(name: "SolidIO", targets: ["SolidCore"]),
+    .library(name: "SolidIO", targets: ["SolidIO"]),
     .library(name: "SolidNumeric", targets: ["SolidNumeric"]),
     .library(name: "SolidTempo", targets: ["SolidTempo"]),
     .library(name: "SolidURI", targets: ["SolidURI"]),
